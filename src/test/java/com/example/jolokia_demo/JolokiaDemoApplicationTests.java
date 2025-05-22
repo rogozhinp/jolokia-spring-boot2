4f21649c0b7fdbfce6f461d52c40097ea40a8ce9
Pavel Rogozhin <rps@it-expertise.ru>
2025-05-22 20:29:14 +0300

    initial commit

---
+++ src/test/java/com/example/jolokia_demo/JolokiaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jolokia_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JolokiaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
